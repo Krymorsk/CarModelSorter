8d2c5339c2959b8553d817f0bf5565043e44e045
Arish Ahmad <88219916+Krymorsk@users.noreply.github.com>
2024-12-04 09:05:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,19 +16,249 @@ let cars = [];
 // Car Data
 const carData = {
     sonet: {
-        variants: ["HTE", "HTK", "HTX", "GTX"],
+        variants: ["HTE", "HTE(O)", "HTK", "HTK(O)", "HTK+", "Gravity", "HTX", "HTX+", "GTX", "GTX+", "XLine"],
         fuelTypes: ["Petrol", "Diesel"],
         transmission: ["Manual", "Automatic"],
         pricing: {
-            HTE: { Petrol: { Manual: { exShowroom: 799900, tcs: 6600, insurance: 38645, total: 945002 } } },
-            HTK: { Petrol: { Manual: { exShowroom: 902900, tcs: 9026, insurance: 41555, total: 1059152 } } }
-        }
+            HTE: {
+                Petrol: {
+                    Manual: { exShowroom: 799900, tcs: 6600, insurance: 38645, basicKit: 25000, total: 945145 },
+                    Automatic: { exShowroom: 849900, tcs: 7100, insurance: 40000, basicKit: 25000, total: 1026900 }
+                },
+                Diesel: {
+                    Manual: { exShowroom: 859900, tcs: 7300, insurance: 41500, basicKit: 25000, total: 1046700 },
+                    Automatic: { exShowroom: 909900, tcs: 7600, insurance: 43000, basicKit: 25000, total: 1099500 }
+                }
+            },
+            "HTE(O)": {
+                Petrol: {
+                    Manual: { exShowroom: 825900, tcs: 6800, insurance: 39500, basicKit: 25000, total: 966200 },
+                    Automatic: { exShowroom: 875900, tcs: 7100, insurance: 41000, basicKit: 25000, total: 1038600 }
+                },
+                Diesel: {
+                    Manual: { exShowroom: 885900, tcs: 7500, insurance: 42500, basicKit: 25000, total: 1063400 },
+                    Automatic: { exShowroom: 935900, tcs: 7800, insurance: 44000, basicKit: 25000, total: 1117100 }
+                }
+            },
+            HTK: {
+                Petrol: {
+                    Manual: { exShowroom: 902900, tcs: 9026, insurance: 41555, basicKit: 25000, total: 1066481 },
+                    Automatic: { exShowroom: 952900, tcs: 9529, insurance: 43000, basicKit: 25000, total: 1113929 }
+                },
+                Diesel: {
+                    Manual: { exShowroom: 962900, tcs: 9630, insurance: 44000, basicKit: 25000, total: 1132530 },
+                    Automatic: { exShowroom: 1012900, tcs: 10130, insurance: 45500, basicKit: 25000, total: 1181530 }
+                }
+            },
+            "HTK(O)": {
+                Petrol: {
+                    Manual: { exShowroom: 949900, tcs: 9500, insurance: 42000, basicKit: 25000, total: 1101399 },
+                    Automatic: { exShowroom: 999900, tcs: 10000, insurance: 43500, basicKit: 25000, total: 1149399 }
+                },
+                Diesel: {
+                    Manual: { exShowroom: 1019900, tcs: 10200, insurance: 44500, basicKit: 25000, total: 1181599 },
+                    Automatic: { exShowroom: 1069900, tcs: 10700, insurance: 46000, basicKit: 25000, total: 1239599 }
+                }
+            },
+            "HTK+": {
+                Petrol: {
+                    Manual: { exShowroom: 999900, tcs: 10000, insurance: 43000, basicKit: 25000, total: 1157899 },
+                    Automatic: { exShowroom: 1049900, tcs: 10500, insurance: 44500, basicKit: 25000, total: 1206900 }
+                },
+                Diesel: {
+                    Manual: { exShowroom: 1099900, tcs: 11000, insurance: 45500, basicKit: 25000, total: 1270400 },
+                    Automatic: { exShowroom: 1149900, tcs: 11500, insurance: 47000, basicKit: 25000, total: 1319900 }
+                }
+            },
+            Gravity: {
+                Petrol: {
+                    Manual: { exShowroom: 1109900, tcs: 11100, insurance: 45000, basicKit: 25000, total: 1291000 },
+                    Automatic: { exShowroom: 1159900, tcs: 11600, insurance: 47000, basicKit: 25000, total: 1343500 }
+                },
+                Diesel: {
+                    Manual: { exShowroom: 1209900, tcs: 12100, insurance: 48500, basicKit: 25000, total: 1395500 },
+                    Automatic: { exShowroom: 1259900, tcs: 12600, insurance: 50000, basicKit: 25000, total: 1447500 }
+                }
+            },
+            HTX: {
+                Petrol: {
+                    Manual: { exShowroom: 1209900, tcs: 12100, insurance: 46500, basicKit: 25000, total: 1412500 },
+                    Automatic: { exShowroom: 1259900, tcs: 12600, insurance: 48000, basicKit: 25000, total: 1466500 }
+                },
+                Diesel: {
+                    Manual: { exShowroom: 1309900, tcs: 13100, insurance: 49500, basicKit: 25000, total: 1531500 },
+                    Automatic: { exShowroom: 1359900, tcs: 13600, insurance: 51000, basicKit: 25000, total: 1585600 }
+                }
+            },
+           
+        }, 
+    
     },
     seltos: {
-        // Add Seltos details
+        variants: ["HTE", "HTK", "HTK+", "HTX", "Gravity", "HTX+", "GTX", "GTX+", "XLine"],
+        fuelTypes: ["Petrol", "Diesel"],
+        transmission: ["Manual", "Automatic"],
+        pricing: {
+            HTE: {
+                Petrol: {
+                    Manual: { exShowroom: 799900, tcs: 6600, insurance: 38645, basicKit: 25000, total: 945145 },
+                    Automatic: { exShowroom: 849900, tcs: 7100, insurance: 40000, basicKit: 25000, total: 1026900 }
+                },
+                Diesel: {
+                    Manual: { exShowroom: 859900, tcs: 7300, insurance: 41500, basicKit: 25000, total: 1046700 },
+                    Automatic: { exShowroom: 909900, tcs: 7600, insurance: 43000, basicKit: 25000, total: 1099500 }
+                }
+            },
+            "HTE(O)": {
+                Petrol: {
+                    Manual: { exShowroom: 825900, tcs: 6800, insurance: 39500, basicKit: 25000, total: 966200 },
+                    Automatic: { exShowroom: 875900, tcs: 7100, insurance: 41000, basicKit: 25000, total: 1038600 }
+                },
+                Diesel: {
+                    Manual: { exShowroom: 885900, tcs: 7500, insurance: 42500, basicKit: 25000, total: 1063400 },
+                    Automatic: { exShowroom: 935900, tcs: 7800, insurance: 44000, basicKit: 25000, total: 1117100 }
+                }
+            },
+            HTK: {
+                Petrol: {
+                    Manual: { exShowroom: 902900, tcs: 9026, insurance: 41555, basicKit: 25000, total: 1066481 },
+                    Automatic: { exShowroom: 952900, tcs: 9529, insurance: 43000, basicKit: 25000, total: 1113929 }
+                },
+                Diesel: {
+                    Manual: { exShowroom: 962900, tcs: 9630, insurance: 44000, basicKit: 25000, total: 1132530 },
+                    Automatic: { exShowroom: 1012900, tcs: 10130, insurance: 45500, basicKit: 25000, total: 1181530 }
+                }
+            },
+            "HTK(O)": {
+                Petrol: {
+                    Manual: { exShowroom: 949900, tcs: 9500, insurance: 42000, basicKit: 25000, total: 1101399 },
+                    Automatic: { exShowroom: 999900, tcs: 10000, insurance: 43500, basicKit: 25000, total: 1149399 }
+                },
+                Diesel: {
+                    Manual: { exShowroom: 1019900, tcs: 10200, insurance: 44500, basicKit: 25000, total: 1181599 },
+                    Automatic: { exShowroom: 1069900, tcs: 10700, insurance: 46000, basicKit: 25000, total: 1239599 }
+                }
+            },
+            "HTK+": {
+                Petrol: {
+                    Manual: { exShowroom: 999900, tcs: 10000, insurance: 43000, basicKit: 25000, total: 1157899 },
+                    Automatic: { exShowroom: 1049900, tcs: 10500, insurance: 44500, basicKit: 25000, total: 1206900 }
+                },
+                Diesel: {
+                    Manual: { exShowroom: 1099900, tcs: 11000, insurance: 45500, basicKit: 25000, total: 1270400 },
+                    Automatic: { exShowroom: 1149900, tcs: 11500, insurance: 47000, basicKit: 25000, total: 1319900 }
+                }
+            },
+            Gravity: {
+                Petrol: {
+                    Manual: { exShowroom: 1109900, tcs: 11100, insurance: 45000, basicKit: 25000, total: 1291000 },
+                    Automatic: { exShowroom: 1159900, tcs: 11600, insurance: 47000, basicKit: 25000, total: 1343500 }
+                },
+                Diesel: {
+                    Manual: { exShowroom: 1209900, tcs: 12100, insurance: 48500, basicKit: 25000, total: 1395500 },
+                    Automatic: { exShowroom: 1259900, tcs: 12600, insurance: 50000, basicKit: 25000, total: 1447500 }
+                }
+            },
+            HTX: {
+                Petrol: {
+                    Manual: { exShowroom: 1209900, tcs: 12100, insurance: 46500, basicKit: 25000, total: 1412500 },
+                    Automatic: { exShowroom: 1259900, tcs: 12600, insurance: 48000, basicKit: 25000, total: 1466500 }
+                },
+                Diesel: {
+                    Manual: { exShowroom: 1309900, tcs: 13100, insurance: 49500, basicKit: 25000, total: 1531500 },
+                    Automatic: { exShowroom: 1359900, tcs: 13600, insurance: 51000, basicKit: 25000, total: 1585600 }
+                }
+            },
+           
+        }, 
+    
     },
     carens: {
-        // Add Carens details
+        variants: ["Premium", "Premium (O)", "Gravity", "Prestige", "Prestige (O)", "Prestige Plus", "Prestige Plus (O)", "Luxury", "Luxury Plus", "XLine"],
+        fuelTypes: ["Petrol", "Diesel"],
+        transmission: ["Manual", "Automatic"],
+        pricing: {
+            HTE: {
+                Petrol: {
+                    Manual: { exShowroom: 799900, tcs: 6600, insurance: 38645, basicKit: 25000, total: 945145 },
+                    Automatic: { exShowroom: 849900, tcs: 7100, insurance: 40000, basicKit: 25000, total: 1026900 }
+                },
+                Diesel: {
+                    Manual: { exShowroom: 859900, tcs: 7300, insurance: 41500, basicKit: 25000, total: 1046700 },
+                    Automatic: { exShowroom: 909900, tcs: 7600, insurance: 43000, basicKit: 25000, total: 1099500 }
+                }
+            },
+            "HTE(O)": {
+                Petrol: {
+                    Manual: { exShowroom: 825900, tcs: 6800, insurance: 39500, basicKit: 25000, total: 966200 },
+                    Automatic: { exShowroom: 875900, tcs: 7100, insurance: 41000, basicKit: 25000, total: 1038600 }
+                },
+                Diesel: {
+                    Manual: { exShowroom: 885900, tcs: 7500, insurance: 42500, basicKit: 25000, total: 1063400 },
+                    Automatic: { exShowroom: 935900, tcs: 7800, insurance: 44000, basicKit: 25000, total: 1117100 }
+                }
+            },
+            HTK: {
+                Petrol: {
+                    Manual: { exShowroom: 902900, tcs: 9026, insurance: 41555, basicKit: 25000, total: 1066481 },
+                    Automatic: { exShowroom: 952900, tcs: 9529, insurance: 43000, basicKit: 25000, total: 1113929 }
+                },
+                Diesel: {
+                    Manual: { exShowroom: 962900, tcs: 9630, insurance: 44000, basicKit: 25000, total: 1132530 },
+                    Automatic: { exShowroom: 1012900, tcs: 10130, insurance: 45500, basicKit: 25000, total: 1181530 }
+                }
+            },
+            "HTK(O)": {
+                Petrol: {
+                    Manual: { exShowroom: 949900, tcs: 9500, insurance: 42000, basicKit: 25000, total: 1101399 },
+                    Automatic: { exShowroom: 999900, tcs: 10000, insurance: 43500, basicKit: 25000, total: 1149399 }
+                },
+                Diesel: {
+                    Manual: { exShowroom: 1019900, tcs: 10200, insurance: 44500, basicKit: 25000, total: 1181599 },
+                    Automatic: { exShowroom: 1069900, tcs: 10700, insurance: 46000, basicKit: 25000, total: 1239599 }
+                }
+            },
+            "HTK+": {
+                Petrol: {
+                    Manual: { exShowroom: 999900, tcs: 10000, insurance: 43000, basicKit: 25000, total: 1157899 },
+                    Automatic: { exShowroom: 1049900, tcs: 10500, insurance: 44500, basicKit: 25000, total: 1206900 }
+                },
+                Diesel: {
+                    Manual: { exShowroom: 1099900, tcs: 11000, insurance: 45500, basicKit: 25000, total: 1270400 },
+                    Automatic: { exShowroom: 1149900, tcs: 11500, insurance: 47000, basicKit: 25000, total: 1319900 }
+                }
+            },
+            Gravity: {
+                Petrol: {
+                    Manual: { exShowroom: 1109900, tcs: 11100, insurance: 45000, basicKit: 25000, total: 1291000 },
+                    Automatic: { exShowroom: 1159900, tcs: 11600, insurance: 47000, basicKit: 25000, total: 1343500 }
+                },
+                Diesel: {
+                    Manual: { exShowroom: 1209900, tcs: 12100, insurance: 48500, basicKit: 25000, total: 1395500 },
+                    Automatic: { exShowroom: 1259900, tcs: 12600, insurance: 50000, basicKit: 25000, total: 1447500 }
+                }
+            },
+            HTX: {
+                Petrol: {
+                    Manual: { exShowroom: 1209900, tcs: 12100, insurance: 46500, basicKit: 25000, total: 1412500 },
+                    Automatic: { exShowroom: 1259900, tcs: 12600, insurance: 48000, basicKit: 25000, total: 1466500 }
+                },
+                Diesel: {
+                    Manual: { exShowroom: 1309900, tcs: 13100, insurance: 49500, basicKit: 25000, total: 1531500 },
+                    Automatic: { exShowroom: 1359900, tcs: 13600, insurance: 51000, basicKit: 25000, total: 1585600 }
+                }
+            },
+           
+        }, 
+    
+    },
+    EV6: {
+        variants: ["GT", "GT (AWD)"],
+        fuelTypes: ["Electric"],
+        transmission: ["Automatic"],
+        pricing: {
+           
+        }
     }
 };
 
